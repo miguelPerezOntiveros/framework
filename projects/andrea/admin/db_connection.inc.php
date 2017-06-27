65cdfa5cae9b2b00db4187cc35fc0ddce84c0d88
miguel <miguel@miguels-MacBook-Pro.local>
2017-06-27 01:16:32 -0500

    removed projects

--- projects/andrea/admin/db_connection.inc.php
+++
@@ -1,5 +0,0 @@
-<?php
-	$conn = new mysqli('127.0.0.1', 'root', 'x1X48bc0Wsm1.sp25', 'andrea');
-	if ($conn->connect_errno)
-		exit('Failed to connect to MySQL: ('.$conn->connect_errno.')'.$conn->connect_error);
-?>
--- projects/andrea/admin/session.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-	session_name('andrea');
-	session_start();
-	if(!isset($_SESSION['userName']) && basename($_SERVER['PHP_SELF']) != 'logIn.php'){
-		header('Location: logIn.php');
-		exit();
-	}
-?>
