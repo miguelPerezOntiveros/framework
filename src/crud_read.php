cde71740b790e6bd6f45f75ebe4d56f11a5497f7
miguel perez <miguel.perez.ontiveros@gmail.com>
2019-02-24 02:25:13 -0600

    using PDO on crud_read.php now

--- src/crud_read.php
+++ src/crud_read.php
@@ -1,25 +1,25 @@
 <?php
 	error_reporting(E_ALL ^ E_NOTICE); 
-	isset($_GET['table']) || exit(json_encode((object) ["error" => "No such table."]));
 	
 	if($_GET['project'] == 'mike_maker')
 		require '../config.inc.php';
 	else
 		require '../projects/'.$_GET['project'].'/admin/config.inc.php';
 
+	// Validating table permissions
 	if($config[$_GET['table']]['_permissions']['read'] != '-'){
 		require 'session.inc.php';
 
-		error_log('read permissions for this "'.$_GET['table'].'" table: '.$config[$_GET['table']]['_permissions']['read']);
-		// Checking table permissions
-		if(!preg_match('/'.$config[$_GET['table']]['_permissions']['read'].'/', $_SESSION['type']))
+		error_log('read permissions table "'.$_GET['table'].'": '.$config[$_GET['table']]['_permissions']['read'].'| session: '.$_SESSION['type']);
+		if(!isset($config[$_GET['table']]) || !preg_match('/'.$config[$_GET['table']]['_permissions']['read'].'/', $_SESSION['type']))
 			exit(json_encode((object) ["error" => "No such table."]));
 	}
 
 	// Checking column permissions
 	$tablesToJoin = [$_GET['table']];
-	$joinRules = ['1'];
 	$allowedColumns = [$_GET['table'].'.id'];
+	$joinRules = ['1'];
+	$bindings = [];
 	if(isset($_GET['show'])) { // only asking for 'show' column
 		if($config[$_GET['table']][$config[$_GET['table']]['_show']]['permissions_read'] == '-' || preg_match('/'.$config[$_GET['table']][$config[$_GET['table']]['_show']]['permissions_read'].'/', $_SESSION['type']))
 			$allowedColumns[] = $_GET['table'].'.'.$config[$_GET['table']]['_show'];
@@ -41,38 +41,40 @@
 					$joinRules[] = $_GET['table'].'.'.$column_key.' = '.$otherTableAlias.'.id';
 				}
 				else if($column['type'] == 'boolean')
-					$allowedColumns[] = 'IF('.$_GET['table'].'.'.$column_key.', "1-Yes", "0-No") as '.$column_key;
+					$allowedColumns[] = 'IF('.$_GET['table'].'.'.$column_key.', "1-Yes", "0-No") as '.$_GET['table'].'.'.$column_key;
 				else
 					$allowedColumns[] = $_GET['table'].'.'.$column_key;
 		}
 	}
-	if(isset($_GET['id']))
-		$joinRules[] = $_GET['table'].'.id = '.$_GET['id'];
-
-	if(isset($_GET['where']))
-		$joinRules[] = $_GET['table'].'.'.$_GET['where'].' = "'.$_GET['equals'].'"';
-
-	if(isset($_GET['columns']))
-		$joinRules[] = '0';
-
 	if(count($allowedColumns) <= 1)
 		exit(json_encode((object) ["error" => "No such table."]));
 
-	//Executing Query
+	if(isset($_GET['id'])){
+		$joinRules[] = $_GET['table'].'.id = ?';
+		$bindings[] = $_GET['id'];
+	}
+
+	if(isset($_GET['where']) && array_search($_GET['where'], $allowedColumns) !== false){
+		$joinRules[] = $_GET['where'].' = ?';
+		$bindings[] = $_GET['equals'];
+	}
+
+	if(isset($_GET['columns']))
+		$joinRules[] = '0';
+	
+	//Executing PDO
 	require 'db_connection.inc.php';
 	$data = array();
 	$columns = array();
 	$sql = 'SELECT '.implode(', ', $allowedColumns).' FROM '.implode(', ', $tablesToJoin).' WHERE '.implode(' and ', $joinRules).';';	
 	error_log('SQL - '.$config['_projectName'].' - ' .$sql);
-	if($result = $conn->query($sql)){
-		while ($column = $result->fetch_field()){
-			$columns[] = (object)[$column->name, $config[$_GET['table']][$column->name]['type'], $config[$_GET['table']][$column->name]['_show'], $config[$_GET['table']][$column->name]['select']]; 
-		}
-		while($row = $result->fetch_array(MYSQLI_NUM))
-			$data[] = $row;
-	}
-	//Executin Query 
-
+	$stmt = $pdo->prepare($sql);
+	error_log('bindings: '.implode(', ', $bindings));
+	$stmt->execute($bindings);
+	foreach(range(0, $stmt->columnCount() - 1) as $i)
+		$columns[] = (object)[$stmt->getColumnMeta($i)['name'], $config[$_GET['table']][$stmt->getColumnMeta($i)['name']]['type'], $config[$_GET['table']][$stmt->getColumnMeta($i)['name']]['_show'], $config[$_GET['table']][$stmt->getColumnMeta($i)['name']]['select']]; 
+	while($row = $stmt->fetch(PDO::FETCH_NUM))
+		$data[] = $row;
 
 	//Possible extension of the service
 	$postfix = 'r';
